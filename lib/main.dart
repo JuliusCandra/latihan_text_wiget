0b03847efc935dc674fa0fd51560bdee4a42d60e
FlutLab.io <>
2021-05-23 15:23:59 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,66 +6,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Candra Julius Sinaga"),
-          leading: Icon(Icons.home),
-          actions: <Widget>[
-            Icon(Icons.search)
-          ],
-          actionsIconTheme: IconThemeData(color: Colors.yellow),
-          backgroundColor: Colors.pinkAccent,
-          bottom: PreferredSize(
-              child: Container(
-                color: Colors.orange,
-                height: 4.0,
-              ),
-              preferredSize: Size.fromHeight(4.0)),
-          centerTitle: true,
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            leading: Icon(Icons.home),
+            title: Text('Candra Julius Sinaga'),
+            actions: <Widget>[
+              Icon(Icons.search),
+            ],
+            actionsIconTheme: IconThemeData(color: Colors.pinkAccent),
+            bottom: PreferredSize(child: Container(color: Colors.blue, height: 4.0), preferredSize: Size.fromHeight(4.0)),
+            centerTitle: true,
+          ),
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.pinkAccent,
+        floatingActonButton: FloatingActionButton(
+          backgroundColor: Colors.yellow,
           child: Text("+", style: new TextStyle(fontSize: 35, color: Colors.black)),
-          onPressed: () {
-            print('Candra Julius Sinaga');
-          },
         ),
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(color: Colors.redAccent, shape: BoxShape.circle),
-          ),
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(color: Colors.pinkAccent, shape: BoxShape.circle),
-          ),
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(color: Colors.blueAccent, shape: BoxShape.circle),
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-            Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(color: Colors.redAccent, shape: BoxShape.circle),
+        debugShowCheckedModeBanner: false,
+        body: Container(
+          margin: EdgeInsets.all(20),
+          color: Colors.pinkAccent,
+          padding: EdgeInsets.all(10),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.lightBlue,
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.red,
+                  Colors.blue
+                ],
+              ),
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: <BoxShadow>[
+                BoxShadow(blurRadius: 10),
+              ],
             ),
-            Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(color: Colors.pinkAccent, shape: BoxShape.circle),
-            ),
-            Container(
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(color: Colors.blueAccent, shape: BoxShape.circle),
-            ),
-          ])
-        ]),
-      ),
-      debugShowCheckedModeBanner: false,
-    );
+          ),
+        ));
   }
 }
