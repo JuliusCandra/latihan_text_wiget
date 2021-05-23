992e23ec30ca64cdabf5dd55ff41ea2d02f86acd
FlutLab.io <>
2021-05-23 17:10:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     debugShowCheckedModeBanner: false,
-     home: Scaffold(
-     appBar: AppBar(
-       title: Text("Candra Julius Sinaga").
-       backgroundColor: Colors.red,
-       centerTitle: true,
-       leading: Icon(Icons.home),
-     ),
-     body: null,
-     ),
-
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Candra Julius Sinaga"),
+          backgroundColor: Colors.red,
+          centerTitle: true,
+          leading: Icon(Icons.home),
+        ),
+        body: null,
+      ),
     );
   }
 }
