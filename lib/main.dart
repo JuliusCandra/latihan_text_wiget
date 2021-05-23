f6b4ce7edff7a8bf123102750e5c137ef2103c2f
FlutLab.io <>
2021-05-23 16:12:34 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
             color: Colors.black,
           ),
         ),
+        centerTitle: true,
       ),
       debugShowCheckedModeBanner: false,
     );
