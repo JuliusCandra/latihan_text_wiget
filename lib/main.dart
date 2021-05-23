2c81749df1caba5eb0b429d91ff9d5b0381ccf7c
FlutLab.io <>
2021-05-23 07:30:23 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,36 +6,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-            leading: Icon(Icons.home),
-            title: Text("Candra Julius Sinaga"),
-            actions: <Widget>[
-              Icon(Icons.search),
-            ],
-            actionsIconTheme: IconThemeData(color: Colors.yellow),
-            backgroundColor: Colors.redAccent,
-            bottom: PreferredSize(child: Container(color: Colors.orange, height: 4.0), preferredSize: Size.fromHeight(4.0)),
-            centerTitle: true),
+        home: Scaffold(
+      appBar: AppBar(
+          leading: Icon(Icons.home),
+          title: Text("Candra Julius Sinaga"),
+          actions: <Widget>[
+            Icon(Icons.search),
+          ],
+          actionsIconTheme: IconThemeData(color: Colors.yellow),
+          backgroundColor: Colors.redAccent,
+          bottom: PreferredSize(child: Container(color: Colors.orange, height: 4.0), preferredSize: Size.fromHeight(4.0)),
+          centerTitle: true),
 
-        // Perubahan baru
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.red,
-            child: Text(
-              "+",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-              ),
+      // Perubahan baru
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          child: Text(
+            "+",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.black,
             ),
-            onPressed: () {
-              for (int i = 0; i < 3; i++) {
-                print("Candra Julius Sinaga");
-              }
-            }),
-        body: 
+          ),
+          onPressed: () {
+            for (int i = 0; i < 3; i++) {
+              print("Candra Julius Sinaga");
+            }
+          }),
+      body: null,
       debugShowCheckedModeBanner: false,
-      )
-    );
+    ));
   }
 }
