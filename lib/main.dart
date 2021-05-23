ab4d4040485f7ffdaa2d1812eb057af53190f0f1
FlutLab.io <>
2021-05-23 16:17:00 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,25 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Candra Julius Sinaga"),
-          leading: Icon(Icons.home),
-          backgroundColor: Colors.pinkAccent,
-          actions: <Widget>[
-            Icon(Icons.search)
-          ],
-          actionsIconTheme: IconThemeData(
-            color: Colors.black,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Candra Julius Sinaga"),
+            leading: Icon(Icons.home),
+            backgroundColor: Colors.pinkAccent,
+            actions: <Widget>[
+              Icon(Icons.search)
+            ],
+            actionsIconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+            centerTitle: true,
           ),
-          centerTitle: true,
         ),
-      ),
-      debugShowCheckedModeBanner: false,
-    );
+        debugShowCheckedModeBanner: false,
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.red,
+            child: Text("+", new TextStyle(fontSize: 35), onPressed: () {
+              return Text("Candra Julius Sinaga", style: new TextStyle(fontSize: 35, color: Colors.red, fontWeight: FontWeight.bold));
+            })));
   }
 }
