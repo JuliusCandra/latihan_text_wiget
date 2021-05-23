6f5b230f62176d587f0f6ed00865d7c29ee5a861
FlutLab.io <>
2021-05-23 15:57:06 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,43 +6,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          leading: Icon(Icons.home),
-          title: Text('Candra Julius Sinaga'),
-          actions: <Widget>[
-            Icon(Icons.search),
-          ],
-          actionsIconTheme: IconThemeData(color: Colors.pinkAccent),
-          bottom: PreferredSize(child: Container(color: Colors.blue, height: 4.0), preferredSize: Size.fromHeight(4.0)),
-          centerTitle: true,
-        ),
-      ),
-      // Perubahan baru
-      debugShowCheckedModeBanner: false,
-      body: Container(
-        margin: EdgeInsets.all(20),
-        color: Colors.pinkAccent,
-        padding: EdgeInsets.all(10),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.lightBlue,
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.red,
-                Colors.blue
-              ],
-            ),
-            borderRadius: BorderRadius.circular(30),
-            boxShadow: <BoxShadow>[
-              BoxShadow(blurRadius: 10),
-            ],
-          ),
-        ),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+      title: Text("Candra Julius Sinaga"),
+      leading: Icon(Icons.home),
+      backgroundColor: Colors.pinkAccent,
+      actions: <Widget>[
+        Icon(Icons.search)
+      ],
+      actionIconsTheme: IconThemeData(color: Colors.yellow),
+      bottom: PreferredSize(child: Container(color: Colors.yellow, height: 4.0)),
+      preferredSize: Size.fromHeight(4.0),
+    )));
   }
 }
