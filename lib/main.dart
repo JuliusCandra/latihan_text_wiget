1c5e9c9007845f8b3dd0052d3980871e6987b58b
FlutLab.io <>
2021-05-23 04:39:17 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,19 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Widget> widgets = [];
-
-  _MyAppState() {
-    for (int i = 0; i < 15; i++)
-      widgets.add(Text("Data ke - " + i.toString(),
-          style: new TextStyle(
-            fontSize: 35,
-          )));
-  }
-
-  final String name = "Candra Julius Sinaga";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+        debugCheckedBanner: false,
+        home: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.red,
             leading: Icon(Icons.home),
             title: Text(name),
           ),
-          body: ListView(children: widgets)),
-    );
+          body: ListView(
+            children: widgets,
+          ),
+        ));
   }
 }
