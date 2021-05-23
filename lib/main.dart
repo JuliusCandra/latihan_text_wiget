fc598aa06ba635612db68a3bc21c539611152c36
FlutLab.io <>
2021-05-23 16:00:53 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-      title: Text("Candra Julius Sinaga"),
-      leading: Icon(Icons.home),
-      backgroundColor: Colors.pinkAccent,
-      actions: <Widget>[
-        Icon(Icons.search)
-      ],
-      actionsIconTheme: IconThemeData(color: Colors.yellow),
-      bottom: PreferredSize(child: Container(color: Colors.yellow, height: 4.0)),
-      preferredSize: Size.fromHeight(4.0),
-    )));
+      appBar: AppBar(
+          title: Text("Candra Julius Sinaga"),
+          leading: Icon(Icons.home),
+          backgroundColor: Colors.pinkAccent,
+          actions: <Widget>[
+            Icon(Icons.search)
+          ],
+          actionsIconTheme: IconThemeData(color: Colors.yellow),
+          bottom: PreferredSize(child: Container(color: Colors.yellow, height: 4.0)),
+          preferredSize: Size.fromHeight(4.0)),
+      centerTitle: true,
+    ));
   }
 }
