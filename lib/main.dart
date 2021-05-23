ce9fcb9d0c4e86304185957ae3be82fabf538073
FlutLab.io <>
2021-05-23 16:33:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -6,35 +6,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Candra Julius Sinaga"),
-          leading: Icon(Icons.home),
-          backgroundColor: Colors.blue,
-          actions: <Widget>[
-            Icon(Icons.search)
-          ],
-          actionsIconTheme: IconThemeData(
-            color: Colors.black,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Candra Julius Sinaga"),
+            leading: Icon(Icons.home),
+            backgroundColor: Colors.blue,
+            actions: <Widget>[
+              Icon(Icons.search)
+            ],
+            actionsIconTheme: IconThemeData(
+              color: Colors.black,
+            ),
+            bottom: PreferredSize(
+              child: Container(color: Colors.yellow, height: 4.0),
+              preferredSize: Size.fromHeight(4.0),
+            ),
+            centerTitle: true,
           ),
-          bottom: PreferredSize(
-            child: Container(color: Colors.yellow, height: 4.0),
-            preferredSize: Size.fromHeight(4.0),
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.red,
+            child: Text(
+              "+",
+              style: new TextStyle(fontSize: 35, color: Colors.black),
+            ),
+            onPressed: () {
+              print("Candra Julius Sinaga");
+            },
           ),
-          centerTitle: true,
         ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.red,
-          child: Text(
-            "+",
-            style: new TextStyle(fontSize: 35, color: Colors.black),
-          ),
-          onPressed: () {
-            print("Candra Julius Sinaga");
-          },
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-    );
+        debugShowCheckedModeBanner: false,
+        body: Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(20),
+            color: Colors.red,
+            child: Container(
+              height: 100,
+              width: 100,
+              margin: EdgeInsets.only(top: 20, bottom: 10),
+              padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+            )));
   }
 }
