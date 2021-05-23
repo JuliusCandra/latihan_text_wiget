19f9fc4ac91770b85658e1e928d96f807617d5b3
FlutLab.io <>
2021-05-23 14:52:14 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
             height: 50,
             decoration: BoxDecoration(color: Colors.blueAccent, shape: BoxShape.circle),
           ),
-          Row(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: <Widget>[
             Container(
               width: 50,
               height: 50,
