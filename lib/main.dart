80cafcbc1fe9bc56f5a480a578eef16f2ff3f8b4
FlutLab.io <>
2021-05-23 05:01:25 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  List<Widget> widgets = [];
-
-  final String name = "Candra Julius Sinaga";
-
-  _MyAppState() {
-    for (int i = 1; i < 101; i++) widgets.add(Text("Data ke - " + i.toString(), style: new TextStyle(fontSize: 35)));
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.red,
+      home: Scaffold(
+        appBar: AppBar(
             leading: Icon(Icons.home),
-            title: Text(name),
-          ),
-          body: ListView(
-            children: widgets,
-          ),
-        ));
+            backgroundColor: Colors.red,
+            title: Text("Candra Julius Sinaga"),
+            actions: <Widget>[
+              Icon(Icons.home),
+              Icon(Icons.search),
+            ],
+            actionsIconTheme: IconThemeData(color: Colors.yellow),
+            backgroundColor: Colors.pinkAccent,
+            bottom: PreferredSize(child: Container(color: Colors.orange, height: 4.0), preferredSize: Size.fromHeight(4.0)),
+            centerTitle: true),
+        // Perubahan baru
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.pinkAccent,
+          child: Text("+"),
+          onPressed: () {},
+        ),
+        body: null,
+      ),
+      debugCheckedModeBanner: false,
+    );
   }
 }
