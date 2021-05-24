a0f33db0ab0d08b0d6bd90f594ea4879102be8d4
FlutLab.io <>
2021-05-24 12:57:07 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,120 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: "Belajar Form Flutter",
+    home: BelajarForm(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class BelajarForm extends StatefulWidget {
+  @override
+  _BelajarFormState createState() => _BelajarFormState();
+}
+
+class _BelajarFormState extends State<BelajarForm> {
+  final _formKey = GlobalKey<FormState>();
+  double nilaiSlider = 1;
+  bool nilaiCheckBox = false;
+  bool nilaiSwitch = true;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Candra Julius Sinaga"),
-          backgroundColor: Colors.red,
-          leading: Icon(Icons.home),
-        ),
-        body: Container(
-          // Menggunakan Column Karena Ada Dua Objek Yang Ingin Dipisahkan
-          // Pertama adalah header beserta summarynya dan yang kedua adalah history
-          child: Column(
-            children: <Widget>[
-              // Kita menggunakan Stack untuk mengatur position header serta container untuk summarynya
-              Stack(children: <Widget>[
-                backgroundHeader(),
-                summaryCash(),
-              ]),
-              // Bagian ini akan menampilkan history pengeluaran
-              cardDetail('Bonus', 'Dapat', '500.000', 'in'),
-              cardDetail('Beli Baju', 'Baju Kemeja Kantor', '250.000', 'out'),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BelajarFlutter.com"),
+      ),
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    decoration: new InputDecoration(
+                      hintText: "contoh: Susilo Bambang",
+                      labelText: "Nama Lengkap",
+                      icon: Icon(Icons.people),
+                      border: OutlineInputBorder(borderRadius: new BorderRadius.circular(5.0)),
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Nama tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    obscureText: true,
+                    decoration: new InputDecoration(
+                      labelText: "Password",
+                      icon: Icon(Icons.security),
+                      border: OutlineInputBorder(borderRadius: new BorderRadius.circular(5.0)),
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Password tidak boleh kosong';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                CheckboxListTile(
+                  title: Text('Belajar Dasar Flutter'),
+                  subtitle: Text('Dart, widget, http'),
+                  value: nilaiCheckBox,
+                  activeColor: Colors.deepPurpleAccent,
+                  onChanged: (value) {
+                    setState(() {
+                      nilaiCheckBox = value;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  title: Text('Backend Programming'),
+                  subtitle: Text('Dart, Nodejs, PHP, Java, dll'),
+                  value: nilaiSwitch,
+                  activeTrackColor: Colors.pink[100],
+                  activeColor: Colors.red,
+                  onChanged: (value) {
+                    setState(() {
+                      nilaiSwitch = value;
+                    });
+                  },
+                ),
+                Slider(
+                  value: nilaiSlider,
+                  min: 0,
+                  max: 100,
+                  onChanged: (value) {
+                    setState(() {
+                      nilaiSlider = value;
+                    });
+                  },
+                ),
+                RaisedButton(
+                  child: Text(
+                    "Submit",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  color: Colors.blue,
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {}
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-// Membuat Card Untuk History Pengeluaran
-Widget cardDetail(title, description, price, type) {}
-
-// Membuat Container Untuk Summary Pengeluaran dan Pemasukan
-Widget summaryCash() {}
-
-// Untuk membuat header
-Widget backgroundHeader() {
-  // Kita buat Container Dengan Tinggi sebesar 300,dan lebar sejauh yang bisa dijangkau
-  // BoxDecorationnya kita set warna pinkaccent dan pada bagian bawah kiri-kanan
-  // dibuat Lengkungan
-  return Container(
-    height: 300,
-    width: double.infinity,
-    decoration: BoxDecoration(
-      color: Colors.pinkAccent,
-      borderRadius: BorderRadius.only(
-        bottomLeft: Radius.circular(30),
-        bottomRight: Radius.circular(30),
-      ),
-    ),
-    // Adapun Child Dari Container Diatas Kita Atur Posisinya menggunakan padding
-    child: Padding(
-      // Paddingnya Di set hanya untuk top dan left
-      padding: const EdgeInsets.only(top: 60, left: 20),
-      // Karena kita akan menampilkan lebihd dari 1 objek susun kebawah
-      // Maka kita menggunakan Column
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          // Bagian ini normal hanya menampilkan teks dan style
-          Text(
-            "Candra Julius Sinaga",
-            style: new TextStyle(fontSize: 35, color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            "082311558341",
-            style: new TextStyle(
-              fontSize: 20,
-              color: Colors.black,
-            ),
-          ),
-        ],
-      ),
-    ),
-  );
-}
-
 /*
 Kesimpulannya
 Card adalah sebuah widget yang menjadi membentuk kartu
