4e62219705d9c03bbadf06184078dbb37ff0f2b4
FlutLab.io <>
2021-05-24 01:52:54 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Candra Julius Sinaga"),
-            backgroundColor: Colors.red,
-            leading: Icon(Icons.home),
-          ),
-          floatingActionButton: FloatingActionButton(
+        appBar: AppBar(
+          title: Text("Candra Julius Sinaga"),
+          backgroundColor: Colors.red,
+          leading: Icon(Icons.home),
+        ),
+        floatingActionButton: FloatingActionButton(
             backgroundColor: Colors.red,
             elevation: 8.0,
-            child: Text("+", style: new TextStyle(fontSize: 25, color: Colors.black)),
-          ),
-          body: Container(
-              child: Card(
-                  child: Column(children: <Widget>[
-            Image.network("https://www.linkedin.com/search/results/people/?firstName=Candra&lastName=Sinaga&origin=SEO_PSERP"),
-            Text("Candra Julius Sinaga")
-          ])))),
+            child: Icon(Icons.add),
+            body: Container(
+                child: Card(
+                    child: Column(children: <Widget>[
+              Image.network("https://www.linkedin.com/search/results/people/?firstName=Candra&lastName=Sinaga&origin=SEO_PSERP"),
+              Text("Candra Julius Sinaga")
+            ])))),
+      ),
     );
   }
 }
